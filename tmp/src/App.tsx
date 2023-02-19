498caed34b273537c229e92308cee75628a3ec85
Florian Zapf <zapforama@googlemail.com>
2023-02-19 14:09:51 +0100

    [transform-to-react-app] React App
    - Update dependencies
    - Add dependencies

--- tmp/src/App.tsx
+++ tmp/src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const Home = lazy(() => import('./app/home/Home'));
+const About = lazy(() => import('./app/about/About'));
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
