dca960a4e1846b9a216c4d9fd9b253961e03145d
Florian Zapf <zapforama@googlemail.com>
2023-02-20 00:20:13 +0100

    [transform-to-react-app] React App
    - Add redux

--- tmp/src/index.tsx
+++ tmp/src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from "react-router-dom";
+import { Provider } from 'react-redux';
 import router from './router';
+import store from './store';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,7 +13,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
